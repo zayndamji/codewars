26600c3e3da6192069a2bd72a26cfc540c5768c7
zaydam1000000 <86502397+zaydam1000000@users.noreply.github.com>
2021-07-26 14:25:25 -0700

    Delete 6kyu (3) directory

--- 6kyu (3)/CreatePhoneNumber.java
+++
@@ -1,8 +0,0 @@
-public class CreatePhoneNumber {
-	public static String createPhoneNumber(int[] numbers) {
-		// Original problem at https://www.codewars.com/kata/525f50e3b73515a6db000b83
-    
-		// Returning the phone number in the correct format
-		return "(" + numbers[0] + numbers[1] + numbers[2] + ") " + numbers[3] + numbers[4] + numbers[5] + "-" + numbers[6] + numbers[7] + numbers[8] + numbers[9];
-	}
-}
