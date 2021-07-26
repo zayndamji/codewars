8166bb943edb7659eb7e06b6fa52423a83490806
zaydam1000000 <86502397+zaydam1000000@users.noreply.github.com>
2021-07-26 08:17:51 -0700

    Update FindMissingLetter.java

--- 6 KYU/FindMissingLetter.java
+++ 6 KYU/FindMissingLetter.java
@@ -1,6 +1,8 @@
 public class FindMissingLetter {
 	public static char findMissingLetter(char[] array) {
-		// This program is written by zaydam1000000
+		// Original problem: https://www.codewars.com/kata/5839edaa6754d6fec10000a2
+		
+		// Declaring variables
 		int firstchar;
     
 		// For loop to check if there is a missing character
