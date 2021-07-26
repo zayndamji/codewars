6c869ad8f54b9ef8421b69b2345a5804af4b1e96
zaydam1000000 <86502397+zaydam1000000@users.noreply.github.com>
2021-07-25 21:08:31 -0700

    Update FindMissingLetter.java

--- FindMissingLetter.java
+++ FindMissingLetter.java
@@ -1,6 +1,8 @@
 public class FindMissingLetter {
 	public static char findMissingLetter(char[] array) {
 		// This program is written by zaydam1000000, original problem at https://www.codewars.com/kata/5839edaa6754d6fec10000a2
+		
+		// Declaring the variable
 		int firstchar;
     
 		// For loop to check if there is a missing character
