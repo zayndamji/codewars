5c052705aef95b4aa4da04f35d571089522b7800
zaydam1000000 <86502397+zaydam1000000@users.noreply.github.com>
2021-07-25 21:30:30 -0700

    Add files via upload

---
+++ 8 KYU/MultiplicationTable.java
@@ -0,0 +1,19 @@
+public class MultiplicationTable {
+	public static String multiTable(int num) {
+		// This program is written by zaydam1000000, original problem at https://www.codewars.com/kata/5a2fd38b55519ed98f0000ce
+      
+		// Declaring variables
+		String returned = "";
+		int multiplied;
+      
+		// Adding values to the string
+		for (int min = 1; min <= 10; min++) {
+			multiplied = min * num;
+			returned = returned + "\n" + min + " * " + num + " = " + multiplied;
+		}
+      
+		// Trimming and returning string
+		returned = returned.trim();
+		return returned;
+	}
+}
