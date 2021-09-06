0a4ea85d468d11b42e92af235537967362a5dea3
zaydam1000000 <86502397+zaydam1000000@users.noreply.github.com>
2021-09-06 14:51:55 -0700

    added 7 kyu project

---
+++ FunctionsOfIntegersOnCatesianPlane.java
@@ -0,0 +1,30 @@
+// 7 KYU
+
+import java.math.BigInteger;
+
+public class FunctionsOfIntegersOnCatesianPlane {
+
+	public static BigInteger sumin(int n) {
+		long val = 0;
+		for (int a = 1; a <= n; a++) {
+			for (int b = 1; b <= n; b++) {
+				val = val + Math.min(a, b);
+			}
+		}
+		return BigInteger.valueOf(val);
+	}
+
+	public static BigInteger sumax(int n) {
+		long val = 0;
+		for (int a = 1; a <= n; a++) {
+			for (int b = 1; b <= n; b++) {
+				val = val + Math.max(a, b);
+			}
+		}
+		return BigInteger.valueOf(val);
+	}
+
+	public static BigInteger sumsum(int n) {
+		return sumin(n).add(sumax(n));
+	}
+}
