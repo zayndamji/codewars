fb48803d415a550aea90f22396411110d6867d43
zaydam1000000 <86502397+zaypers@users.noreply.github.com>
2023-02-02 08:53:00 -0800

    FindNearestSquareNumber ts/js

---
+++ 8 kyu/FindNearestSquareNumber.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/5a805d8cafa10f8b930005ba
+// 8 KYU
+
+function nearestSq(n){
+  return Math.pow(Math.round(Math.sqrt(n)), 2);
+}
---
+++ 8 kyu/FindNearestSquareNumber.ts
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/5a805d8cafa10f8b930005ba
+// 8 KYU
+
+export function nearestSq(n : number) : number {
+  return Math.pow(Math.round(Math.sqrt(n)), 2);
+}
